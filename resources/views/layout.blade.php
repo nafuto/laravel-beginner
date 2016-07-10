cfced886aa650d1e7db1d30a71a00054fa022bb2
Chung Tran <pirate1912@gmail.com>
2016-07-10 19:06:11 +0700

    css and js

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
-	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="/css/style.css">
+
 </head>
 <body>
 
